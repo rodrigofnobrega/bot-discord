8ed10a5eef44d84c1c6defe696c21742cff66424
Rodrigo Nobrega <rofen1@protonmail.com>
2023-08-29 08:38:02 -0300

    Rename class TokenBot to BotConfig

--- src/main/java/com/discord/bot/BotConfig.java
+++ src/main/java/com/discord/bot/BotConfig.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class TokenBot {
+public class BotConfig {
     private Properties getProp() throws IOException {
         Properties props = new Properties();
         FileInputStream file = new FileInputStream(
--- src/main/java/com/discord/bot/Main.java
+++ src/main/java/com/discord/bot/Main.java
@@ -4,13 +4,11 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
 
-import com.discord.bot.TokenBot;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        TokenBot token = new TokenBot();
+        BotConfig token = new BotConfig();
 
         DiscordApi api = new DiscordApiBuilder()
             .setToken(token.getToken())
